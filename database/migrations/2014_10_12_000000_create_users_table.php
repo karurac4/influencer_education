a6d9cd2567a00020aed3f43f7744139625acabc6
shoma <cs.tkst.4strings@gmail.com>
2024-02-27 17:55:11 +0900

    Kuribayashi migrate tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name_kana');
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('profile_image')->nullable();
+            $table->integer('grade_id');
             $table->timestamps();
         });
     }
